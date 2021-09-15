ffef557429cea2a12d1fecea9b7738804adeacd7
ijacab <ijacab@users.noreply.github.com>
2021-09-16 00:07:04 +1000

    Add Card

---
+++ DiscordBot/Games/Models/Card.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Games.Models
+{
+    public class Card
+    {
+        public readonly char Name; //A,2,3,J,K,etc
+        public readonly int[] Values; //2,3,10,10,1/11etc
+        public readonly Face Face;
+
+        public Card(char name, Face face)
+        {
+            Name = name;
+            switch (name)
+            {
+                case 'A':
+                    Values = new int[] { 1, 11 };
+                    break;
+                case 'J':
+                case 'Q':
+                case 'K':
+                    Values = new int[] { 10 };
+                    break;
+                default:
+                    Values = new int[] { Convert.ToInt32(name) };
+                    break;
+            }
+        }
+    }
+
+    public enum Face
+    {
+        Hearts,
+        Diamonds,
+        Clubs,
+        Spades
+    }
+}
